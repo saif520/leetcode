e679a3748c74fbc79c930dc2855ee00234ddb00b
saifuddin <saifuddin2k17@gmail.com>
2022-11-28 10:15:08 +0530

    intersection of two array 2

---
+++ sorting/intersection_of_two_array_2.js
@@ -0,0 +1,70 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+ var intersect = function(nums1, nums2) {
+    let l1=nums1.length;
+    let l2=nums2.length;
+    mergeSort(nums1,0,l1-1);
+    mergeSort(nums2,0,l2-1);
+    
+    let ans=[];
+    let i=0;
+    let j=0;
+    while(i<l1&&j<l2){
+        if(nums1[i]<nums2[j]){
+            i++;
+        }
+        else if(nums1[i]>nums2[j]){
+            j++;
+        }
+        else{
+            ans.push(nums1[i]);
+            i++;
+            j++;
+        }
+    }
+    return ans;
+};
+let merge=function(arr,lb,mid,ub){
+    let i=lb;
+    let j=mid+1;
+    let k=lb;
+    let b=[];
+    while(i<=mid&&j<=ub){
+        if(arr[i]<=arr[j]){
+            b[k]=arr[i];
+            i++;
+            k++;
+        }
+        else{
+            b[k]=arr[j];
+            j++;
+            k++;
+        }
+    }
+    while(j<=ub){
+        b[k]=arr[j];
+        j++;
+        k++;
+    }
+    while(i<=mid){
+        b[k]=arr[i];
+        i++;
+        k++;
+    }
+    for(let k=lb;k<=ub;k++){
+        arr[k]=b[k];
+    }
+}
+
+let mergeSort=function(arr,lb,ub){
+
+    if(lb<ub){
+        let mid=Math.floor((lb+ub)/2);
+        mergeSort(arr,lb,mid);
+        mergeSort(arr,mid+1,ub);
+        merge(arr,lb,mid,ub);
+    }
+}
