a8c88a041e34baaa762491de1f48acf3e49bc0ee
saif520 <saifuddin2k17@gmail.com>
2023-01-21 17:31:07 +0530

    Boarts to Save People

---
+++ sorting/881. Boats to Save People.js
@@ -0,0 +1,60 @@
+/**
+ * @param {number[]} people
+ * @param {number} limit
+ * @return {number}
+ */
+var numRescueBoats = function(people, limit) {
+    mergeSort(people,0,people.length-1);
+    let count=0;
+    let start=0;
+    let end=people.length-1;
+    while(end>=start){
+        if(people[end]+people[start]<=limit){
+            start++;
+        }
+        end--;
+        count++;
+    }
+    return count;
+};
+let mergeSort=function(nums,lb,ub){
+    if(lb==ub){
+        return;
+    }
+    let mid=Math.floor((lb+ub)/2);
+    mergeSort(nums,lb,mid);
+    mergeSort(nums,mid+1,ub);
+    merge(nums,lb,mid,ub);
+}
+let merge=function(nums,lb,mid,ub){
+    let i=lb;
+    let j=mid+1;
+    let k=0;
+    let arr=[];
+    while(i<=mid&&j<=ub){
+        if(nums[i]<=nums[j]){
+            arr.push(nums[i]);
+            i++;
+        }
+        else{
+            arr.push(nums[j]);
+            j++;
+        }
+        k++
+    }
+    while(i<=mid){
+        arr.push(nums[i]);
+        i++;
+        k++;
+    }
+    while(j<=ub){
+        arr.push(nums[j]);
+        j++;
+        k++;
+    }
+    let p=0;
+    for(let idx=lb;idx<=ub;idx++){
+        nums[idx]=arr[p];
+        p++;
+    }
+}
