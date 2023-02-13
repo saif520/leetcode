d6b1c003ae4f25b98f61d3c69626f0892313fd96
saif520 <saifuddin2k17@gmail.com>
2023-02-13 12:34:15 +0530

    2390. Removing Stars From a String

---
+++ stack/2390. Removing Stars From a String.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String removeStars(String s) {
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)!='*'){
+                st.push(s.charAt(i));
+            }
+            else{
+                st.pop();
+            }
+        }
+        if(st.isEmpty())return "";
+        StringBuilder sb=new StringBuilder();
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
