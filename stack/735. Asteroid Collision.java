6f52c533e1a4c6d91bde7262567349c14dd59ec3
saif520 <saifuddin2k17@gmail.com>
2023-02-17 03:25:21 +0530

    735. Asteriod Collision

---
+++ stack/735. Asteroid Collision.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> st =new Stack<>();
+		for(int asteroid : asteroids){
+			if(asteroid>0){
+				st.push(asteroid);
+			}
+			else{
+				while(st.size()>0&&st.peek()>0&&st.peek()<-asteroid){
+					st.pop();
+				}
+				if(st.size()>0&&st.peek()> -asteroid){
+					continue;
+				}
+				else if(st.size()>0&&st.peek()== -asteroid){
+					st.pop();
+				}
+				else{
+					st.push(asteroid);
+				}
+			}
+		}
+		int ans[] =new int[st.size()];
+		for(int i=ans.length-1;i>=0;i--){
+			ans[i]=st.pop();
+		}
+		return ans;
+    }
+}
