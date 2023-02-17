b1073236fc4ef0c542b4d547f7918157844cab07
saif520 <saifuddin2k17@gmail.com>
2023-02-17 12:58:56 +0530

    994. Rotting Oranges

---
+++ queue/994. Rotting Oranges.java
@@ -0,0 +1,60 @@
+class Solution {
+    class Pair{
+        int row;
+        int col;
+        Pair(int r,int c){
+            row=r;
+            col=c;
+        }
+    }
+    public int orangesRotting(int[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+        Queue<Pair> que =new ArrayDeque<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    que.add(new Pair(i,j));
+                }
+            }
+        }
+        int time=0;
+        while(que.size()>0){
+            int size=que.size();
+            while(size>0){
+                Pair rnode=que.remove();
+                int cr=rnode.row;
+                int cc=rnode.col;
+                if(cr-1>=0&&grid[cr-1][cc]==1){
+                    grid[cr-1][cc]=2;
+                    que.add(new Pair(cr-1,cc));
+                }
+                if(cr+1<n&&grid[cr+1][cc]==1){
+                    grid[cr+1][cc]=2;
+                    que.add(new Pair(cr+1,cc));
+                }
+                if(cc-1>=0&&grid[cr][cc-1]==1){
+                    grid[cr][cc-1]=2;
+                    que.add(new Pair(cr,cc-1));
+                }
+                if(cc+1<m&&grid[cr][cc+1]==1){
+                    grid[cr][cc+1]=2;
+                    que.add(new Pair(cr,cc+1));
+                }
+                size--;
+            }
+            time++;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    return -1;
+                }
+            }
+        }
+        if(time==0){
+            return time;
+        }
+        return time-1;
+    }
+}
