01b8076c2bcff253af4d4d4b48f1fb9ec508de4d
saif520 <saifuddin2k17@gmail.com>
2023-02-18 01:14:51 +0530

    239. Sliding Window Maximum

---
+++ Monotonic_queue/239. Sliding Window Maximum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n=nums.length;
+         int[] ans = new int[n - k + 1];
+        int itr = 0;
+
+        Deque<Integer> dq = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            if (dq.size() > 0 && dq.getFirst() == i - k) {
+                dq.removeFirst();
+            }
+
+            while (dq.size() > 0 && nums[dq.getLast()] <= nums[i]) {
+                dq.removeLast();
+            }
+
+            dq.addLast(i);
+
+            if (i >= k - 1) {
+                ans[itr] = nums[dq.getFirst()];
+                itr++;
+            }
+        }
+
+        return ans;
+    }
+}
