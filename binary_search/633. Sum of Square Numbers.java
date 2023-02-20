064f168ec40a266cbaaaed84deb837d43c26ed40
saif520 <saifuddin2k17@gmail.com>
2023-02-20 13:39:49 +0530

    633. Sum of Square Number

---
+++ binary_search/633. Sum of Square Numbers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        long l=0;
+        long r=(int)Math.sqrt(c);
+        while(l<=r){
+            long sum=l*l+r*r;
+            if(sum==c){
+                return true;
+            }
+            else if(sum<c){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        return false;
+    }
+}
