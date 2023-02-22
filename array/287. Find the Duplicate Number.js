72718ca7fadcfa7f26d66f8f41895dff71a1fec2
saif520 <saifuddin2k17@gmail.com>
2023-02-23 03:31:59 +0530

    287. Find the Duplicate Number

---
+++ array/287. Find the Duplicate Number.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    let obj={};
+    for(let i=0;i<nums.length;i++){
+        if(obj[nums[i]]){
+            obj[nums[i]]++;
+        }
+        else{
+            obj[nums[i]]=1;
+        }
+    }
+    console.log(obj);
+    for(let i in obj){
+        if(obj[i]>=2){
+            return i;
+        }
+    }
+};
