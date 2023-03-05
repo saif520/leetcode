b90853c0b1e3d699b520de76ff1fafcd3ab0b393
saif520 <saifuddin2k17@gmail.com>
2023-03-05 10:01:47 +0530

    1812. Determine Color of a Chessboard Square

---
+++ math/1812. Determine Color of a Chessboard Square.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean squareIsWhite(String coordinates) {
+        if(coordinates.charAt(0)%2==0&&coordinates.charAt(1)%2!=0){
+            return true;
+        }
+        else if(coordinates.charAt(0)%2!=0&&coordinates.charAt(1)%2==0){
+            return true;
+        }
+        return false;
+    }
+}
