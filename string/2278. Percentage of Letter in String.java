060ee9d81eb8b33268e3917e759cef99c78c245a
saif520 <saifuddin2k17@gmail.com>
2023-03-06 21:28:01 +0530

    2278. Percentage of Letter in String

---
+++ string/2278. Percentage of Letter in String.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int percentageLetter(String s, char letter) {
+        int count=0;
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)==letter){
+                count++;
+            }
+        }
+        return (count*100)/n;
+    }
+}
