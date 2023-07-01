4c786e0515a94035f8844f6a21434ba97b9f6198
saif520 <saifuddin2k17@gmail.com>
2023-07-01 23:18:56 +0530

    2295. Replace Elements in an Array

---
+++ hashmap/454. 4Sum II.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<A.length;i++){
+            for(int j=0;j<B.length;j++){
+                int sum_AB=A[i]+B[j];
+                map.put(sum_AB,map.getOrDefault(sum_AB,0)+1);
+            }
+        }
+        int ans=0;
+        for(int i=0;i<C.length;i++){
+            for(int j=0;j<D.length;j++){
+                int sum_CD=C[i]+D[j];
+                int sum_AB=-sum_CD;
+                ans+=map.getOrDefault(sum_AB,0);
+            }
+        }
+        return ans;
+    }
+}
