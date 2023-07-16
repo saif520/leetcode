a251eab0389a31812a6015bfe0893428699b0e5b
saif520 <saifuddin2k17@gmail.com>
2023-07-16 13:51:54 +0530

    2778. Sum of Square of Special Elements

---
+++ array/2778. Sum of Squares of Special Elements.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sumOfSquares = function(nums) {
+    let n=nums.length;
+    let sum=0;
+    for(let i=0;i<n;i++){
+        if(n%(i+1)==0){
+            sum+=nums[i]*nums[i];
+        }
+    }
+    return sum;
+};
