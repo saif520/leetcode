ce634b4aafada5c801d3623386e0f15de00ba766
saif520 <saifuddin2k17@gmail.com>
2023-10-27 00:35:03 +0530

    2011. Final Value of Variable After Performing Operations

---
+++ array/2011. Final Value of Variable After Performing Operations.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var finalValueAfterOperations = function(operations) {
+    let ans=0;
+    for(let i=0;i<operations.length;i++){
+        if(operations[i]=="--X"||operations[i]=="X--"){
+            ans--;
+        }
+        else{
+            ans++;
+        }
+    }
+    return ans;
+};
