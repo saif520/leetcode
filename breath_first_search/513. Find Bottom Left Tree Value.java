e0322a9cdadd339aec478ca60704a37fafb81116
saif520 <saifuddin2k17@gmail.com>
2024-02-28 15:05:08 +0530

    513. Find Bottom Left Tree Value

---
+++ breath_first_search/513. Find Bottom Left Tree Value.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+      q.deque([root]);
+
+        while q:
+            node=q.popleft();
+
+            if node.right: q.append(node.right)
+            if node.left: q.append(node.left)
+        return node.val
+    }
+}
