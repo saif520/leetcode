5beee69be2526d432212f312b9e90a6ea75fe042
saif520 <saifuddin2k17@gmail.com>
2024-03-23 08:51:26 +0530

    1656. Design an Order Stream

---
+++ design/1656. Design an Ordered Stream.java
@@ -0,0 +1,24 @@
+class OrderedStream {
+    int pointer;
+    String[] res;
+    public OrderedStream(int n) {
+        pointer=0;
+        res=new String[n];
+    }
+    
+    public List<String> insert(int idKey, String value) {
+        List<String>list=new ArrayList<String>();
+        res[idKey-1]=value;
+        while(pointer<res.length&&res[pointer]!=null){
+            list.add(res[pointer]);
+            pointer++;
+        }
+        return list;
+    }
+}
+
+/**
+ * Your OrderedStream object will be instantiated and called as such:
+ * OrderedStream obj = new OrderedStream(n);
+ * List<String> param_1 = obj.insert(idKey,value);
+ */
