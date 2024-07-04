c22e33e13db07883e4415070a50e551253831fba
saif520 <saifuddin2k17@gmail.com>
2024-07-04 18:23:01 +0530

    2399. Check Distances Between Same Letters

---
+++ array/2399. Check Distances Between Same Letters.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean checkDistances(String s, int[] distance) {
+        int[][] indicesList=new int[26][2];
+
+        for(int[] indices : indicesList){
+            Arrays.fill(indices,-1);
+        }
+
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            int indices[]=indicesList[c-'a'];
+            if(indices[0]==-1){
+                indices[0]=i;
+            }
+            else{
+                indices[1]=i;
+            }
+        }
+
+        for(int i=0;i<distance.length;i++){
+            if(indicesList[i][0]!=-1&&indicesList[i][1]!=-1){
+                int actualDistance=indicesList[i][1]-indicesList[i][0];
+                if(actualDistance-1!=distance[i]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
