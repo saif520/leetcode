83009f5a29df710b4ebd8f2fce9b7a1837b17df6
saif520 <saifuddin2k17@gmail.com>
2024-07-07 10:08:20 +0530

    1518. Water Bottles

---
+++ math/1518. Water Bottles.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numWaterBottles(int nb, int ne) {
+        
+        int ans=nb;
+
+        while(nb>=ne){
+            int t=nb/ne;
+            int r=nb%ne;
+            ans+=t;
+            nb=t+r;
+        }
+        
+        return ans;
+    }
+}
