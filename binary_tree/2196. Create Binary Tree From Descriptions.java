ba3cf177c2a9204498e62eac91b4418a9af102c8
saif520 <saifuddin2k17@gmail.com>
2024-07-15 09:20:58 +0530

    2196. Create Binary Tree From Descriptions

---
+++ binary_tree/2196. Create Binary Tree From Descriptions.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        HashMap<Integer,TreeNode>m=new HashMap<>();
+        HashSet<Integer>vis=new HashSet<>();
+        for(int[] d:descriptions){
+            int p=d[0];
+            int c=d[1];
+            int isLeft=d[2];
+            if(!m.containsKey(p)){
+                m.put(p,new TreeNode(p));
+            }
+            if(!m.containsKey(c)){
+                m.put(c,new TreeNode(c));
+            }
+            if(isLeft==1){
+                m.get(p).left=m.get(c);
+            }
+            else{
+                m.get(p).right=m.get(c);
+            }
+            vis.add(c);
+        }
+        for(Map.Entry<Integer,TreeNode>entry:m.entrySet()){
+            if(!vis.contains(entry.getKey())){
+                return entry.getValue();
+            }
+        }
+        return null;
+    }
+}
