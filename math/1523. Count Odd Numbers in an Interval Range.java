f3d00a2352a9837dc064303dec8bd7724f201bf2
saif520 <saifuddin2k17@gmail.com>
2025-12-07 08:23:46 +0530

    1523. Count Odd Numbers in an Interval Range

---
+++ math/1523. Count Odd Numbers in an Interval Range.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countOdds(int low, int high) {
+        int count=0;
+        if(low%2==0){
+            low=low+1;
+        }
+        while(low<=high){
+            if(low%2!=0){
+                count++;
+            }
+            low+=2;
+        }
+        return count;
+    }
+}
