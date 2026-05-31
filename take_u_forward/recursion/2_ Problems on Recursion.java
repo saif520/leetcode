b8a08ef1fa96054f0a357d619b91b7b695a0f315
saif520 <saifuddin2k17@gmail.com>
2026-05-31 15:56:41 +0530

    Print name N times using Recursion

---
+++ take_u_forward/recursion/2_ Problems on Recursion.java
@@ -0,0 +1,23 @@
+// Print name N times using Recursion
+
+import java.util.Scanner;
+public class Main
+{
+    static void fun(int i,int n){
+        if(i>n){
+            return;
+        }
+        System.out.println("Saifuddin");
+        fun(i+1,n);
+    }
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.print("Enter the the value of N: ");
+		int n=sc.nextInt();
+		
+		fun(1,n);
+		
+	}
+}
