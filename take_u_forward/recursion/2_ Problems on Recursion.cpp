158ea4b32a275e041d14071698c2f535ad38f863
saif520 <saifuddin2k17@gmail.com>
2026-05-31 16:38:32 +0530

    Print 1 to N

--- take_u_forward/recursion/2_ Problems on Recursion.cpp
+++ take_u_forward/recursion/2_ Problems on Recursion.cpp
@@ -1,4 +1,4 @@
-// Print name N times using Recursion
+//Q1. Print name N times using Recursion
 
 #include <iostream>
 
@@ -7,6 +7,29 @@ void fun(int i,int n){
         return;
     }
     std::cout<<"Saifuddin\n";
+    fun(i+1,n);
+}
+
+int main()
+{
+    int n;
+    std::cout<<"Enter the value of N:\t";
+    std::cin>>n;
+    
+    fun(1,n);
+
+    return 0;
+}
+
+//Q2. Print 1 to N
+
+#include <iostream>
+
+void fun(int i,int n){
+    if(i>n){
+        return;
+    }
+    std::cout<<i<<"\n";
     fun(i+1,n);
 }
 
--- take_u_forward/recursion/2_ Problems on Recursion.java
+++ take_u_forward/recursion/2_ Problems on Recursion.java
@@ -21,3 +21,28 @@ public class Main
 		
 	}
 }
+
+
+//Q2. Print 1 to N
+
+import java.util.Scanner;
+public class Main
+{
+    static void fun(int i,int n){
+        if(i>n){
+            return;
+        }
+        System.out.println(i);
+        fun(i+1,n);
+    }
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.print("Enter the the value of N: ");
+		int n=sc.nextInt();
+		
+		fun(1,n);
+		
+	}
+}
